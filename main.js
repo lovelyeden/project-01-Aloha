552a76bd553bf1d0aa20ac867f409531b17f5524
lovely <ldde07@gmail.com>
2019-04-21 01:33:57 -0700

    My 5th commit

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //All of your code goes inside here
+    console.log('working');
+    const flkty = new Flickity( '.main-carousel', {
+      cellAlign: 'left',
+    //   contain: true,
+      wrapAround: true,
+      freeScroll: true
+    });
+    
+
+
+
+
+});//end of document ready
