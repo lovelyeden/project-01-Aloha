8e4e9ff48718b86e6f15f432c4a9590844e705a6
lovely <ldde07@gmail.com>
2019-07-16 23:51:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
-
-    //All of your code goes inside here
     document.addEventListener("DOMContentLoaded", function() {
 
-      //All of your code goes inside here
-      console.log('working');
+      //Carousel 
       const flkty = new Flickity( '.main-carousel', {
-        cellAlign: 'left',
-      
+      cellAlign: 'left',
       });
+      //Email Validation 
+      const subscribe = document.getElementById("subscribe-form");
+      const inputText = document.getElementById("email");
+      subscribe.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let inputVal = inputText.value;
+
+      if (inputVal === "") {
+      alert('Please entered an valid email address!');
+      } else {
+      alert('Thank you for your subscribe!');
+    }
+
+    });
       
+      // Cartt
+      const cartTotal = 0;
+      $("$counter").hide();
+
+      $(".button").on("click", function(event) {
+        event.preventDefault();
+        cartTotal++;
+        $("$counter")
+          .text(cartTotal)
+          .show();
+        console.log(".counter");
+      });
 });//end of document ready
 
 
